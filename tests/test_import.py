f76329b138c0f5c6c65b678cbbae560e63b49dd7
Yassine Housseine <151876208+Yaskoi@users.noreply.github.com>
2026-04-11 00:12:39 +0200

    Update test_import.py

--- tests/test_import.py
+++ tests/test_import.py
@@ -2,7 +2,7 @@ import stratoquant as sq
 import numpy as np
 
 def test_version():
-    assert sq.__version__ == "2.0.0"
+    assert sq.__version__ == "1.0.0"
 
 def test_bs_price():
     price = sq.black_scholes_price(100, 100, 1.0, 0.05, 0.2, 'call')
